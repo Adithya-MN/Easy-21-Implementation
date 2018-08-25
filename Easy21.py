f8c807fca8e57af4296744edae8f4cb286a073a6
Adithya <adithya.niranjan2412982@gmail.com>
2018-08-25 23:43:10 +0530

    Code compiles, writing test cases now. Also need plot function

--- Easy21.py
+++ Easy21.py
@@ -8,8 +8,8 @@ class Env(object):
         self.dealer_score = self.state['dealer_card']
         self.last_reward = 0
         self.reward = 0
-
         self.terminal_state = False
+
 
     def dealer(self, status):
 
@@ -22,7 +22,7 @@ class Env(object):
                 dealer_policy()
             if(self.dealer_sum < 0 or self.dealer_sum > 21 or player_sum > dealer_sum):
                 reward('player')
-            elif(self.dealer_sum = player_sum):
+            elif(self.dealer_sum == player_sum):
                 reward('draw')
                 self.last_reward = 0
             else:
@@ -47,7 +47,7 @@ class Env(object):
                 card = 10
         if colour == 'red':
             self.dealer_sum = self.dealer_sum - card
-        elif colour = 'black':
+        elif colour == 'black':
             self.dealer_sum = self.dealer_sum + card
 
 
@@ -58,36 +58,37 @@ class Env(object):
 
     def step(self):
 
-    dealer_card  = self.state ['dealer_card']
-    player_sum = self.state['player_sum']
-    action = self.state['action']
 
-    if action = 'hit':
+        dealer_card  = self.state ['dealer_card']
+        player_sum = self.state['player_sum']
+        action = self.state['action']
 
-        colour = 'red' if np.random.rand() > (1.0/3) else 'black'
-        card = np.random.randint(1,14)
-        if card > 10:
-            card = 10
+        if action == 'hit':
 
-        if colour == 'red':
-            player_sum = player_sum - card
-        elif colour = 'black':
-            player_sum = player_sum + card
+            colour = 'red' if np.random.rand() > (1.0/3) else 'black'
+            card = np.random.randint(1,14)
+            if card > 10:
+                card = 10
 
-        if (player_sum > 21 or player_sum < 1):
-            self.dealer('player_bust')
-        else:
-            self.dealer('hit')
-    else:
-        self.dealer('stick')
+            if colour == 'red':
+                player_sum = player_sum - card
+            elif colour == 'black':
+                player_sum = player_sum + card
 
-    def reward(self, winner):1
+            if (player_sum > 21 or player_sum < 1):
+                self.dealer('player_bust')
+            else:
+                self.dealer(action)
+        elif(action == 'stick'):
+            self.dealer(action)
+
+    def reward(self, winner):
 
         self.terminal_state = True
-        if(winner = 'player'):
+        if(winner == 'player'):
             self.reward = self.reward + 1
             self.last_reward = 1
 
-        elif(winner = 'dealer'):
+        elif(winner == 'dealer'):
             self.reward = self.reward - 1
             self.last_reward = -1
--- monte_carlo.py
+++ monte_carlo.py
@@ -1,60 +1,58 @@
- from Easy21 import *
- import numpy as np
+from Easy21 import *
+import numpy as np
 
 
- def monte_carlo_policy():
+def monte_carlo_policy():
 
-	 card = np.random.randint(1,14)
-	 if card > 10:
+	card= np.random.randint(1,14)
+	if card > 10:
 		 card = 10
 
-	 state = {dealer_card : card,
+	state= {dealer_card : card,
 			  player_sum : 0,
-			  action = 'hit'}
+			  action : 'hit'}
 
-	 environment = Env(state)
-
-
-	 dealer_card_states = np.arange(1,14)
-	 num_dealer_states = dealer_card_states.shape[0]
-
-	 player_sum_states = np.arange(1, 22)
-	 num_player_states = player_sum_states.shape[0]
-
-	 actions = ['hit', 'stick']
-	 num_actions = len(actions)
-
-	 _value_function = np.zeros((dealer_card_states, num_player_states, num_actions))
-
-	 _freq = np.zeros((dealer_card_states, num_player_states, num_actions))
-
-	 N0 = 100
-
-	 visits = []
-
-	 while (environment.terminal_state == False):
-
-		 dealer_card  = environment.state ['dealer_card']
-		 player_sum = environment.state['player_sum']
-		 action = environment.state['action']
+	environment = Env(state)
 
 
-		 _epsilon =  N0 / (N0 + _np.sum(freq[dealer_card][player_sum]) )
+	dealer_card_states = np.arange(1,14)
+	num_dealer_states = dealer_card_states.shape[0]
 
-		 if(np.random.rand() < _epsilon):
+	player_sum_states = np.arange(1, 22)
+	num_player_states = player_sum_states.shape[0]
+
+	actions = ['hit', 'stick']
+	num_actions = len(actions)
+
+	_value_function = np.zeros((dealer_card_states, num_player_states, num_actions))
+
+	_freq = np.zeros((dealer_card_states, num_player_states, num_actions))
+
+	N0 = 100
+
+	visits = []
+
+	while (environment.terminal_state == False):
+
+		dealer_card  = environment.state ['dealer_card']
+		player_sum = environment.state['player_sum']
+		action = environment.state['action']
+
+
+		_epsilon =  N0 / (N0 + _np.sum(freq[dealer_card][player_sum]) )
+
+		if(np.random.rand() < _epsilon):
 			 action = np.random.choice(['hit', 'stick'])
-		 else:
+		else:
 			 action = np.argmax(_value_function[dealer_card][player_sum])
 			 environment.state['action'] = actions[action]
 
-		 visits.append([dealer_card, player_sum, action])
+		visits.append([dealer_card, player_sum, action])
 
-		 environment.step()
+		environment.step()
 
 	reward = environment.last_reward
 
 	for visit in visits:
 		[ dealer_card, player_sum, action ]= visit
 		_value_function[dealer_card][player_sum][action]  =  __value_function[dealer_card][player_sum][action] + reward/_freq[dealer_card][player_sum][action]
-
-	
